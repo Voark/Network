c62ef595dede4a1bb7bd116b5d9ea7ddc0475e5c
Voark <fredisawesome147@gmail.com>
2020-09-01 16:06:21 -0700

    Added append to history and new TODOs

--- gradient_descent_algorithms.py
+++ gradient_descent_algorithms.py
@@ -1,11 +1,13 @@
-import numpy as cp
+import numpy as np
+#TODO: condense batch, SGD, and mini-batch gradient descent to SGD with variable batch size
+#TODO: Add other optimizers (Adam)
 def list_functions():
     functions = {'batch': batch_gradient_descent, 'stochastic': stochastic_gradient_descent, 'mini batch': mini_batch_gradient_descent}
     return functions
 
 def batch_gradient_descent(network, train_x, train_y, test_x, test_y, epochs):
     for epoch in range(epochs):
-        total_gradient = [[cp.zeros(w.shape) for w in reversed(network.weights)], [cp.zeros(b.shape) for b in reversed(network.biases)]]
+        total_gradient = [[np.zeros(w.shape) for w in reversed(network.weights)], [np.zeros(b.shape) for b in reversed(network.biases)]]
         for x, y in zip(train_x, train_y):
             x, y = network.reshape_data(x, y)
             a, z = network.forward_prop(x)
@@ -17,30 +19,34 @@ def batch_gradient_descent(network, train_x, train_y, test_x, test_y, epochs):
             for j in range(len(total_gradient[i])):
                 total_gradient[i][j] /= train_x.shape[0]
         network.update_weights_and_biases(*total_gradient)
-        print(f'Epoch: {epoch + 1}. Accuracy: {network.test_accuracy(test_x, test_y) * 100}%')
+        acc = network.test_accuracy(test_x, test_y) 
+        print(f'Epoch: {epoch + 1}. Accuracy: {acc * 100}%')
+        network.history.append(acc)
 
 def stochastic_gradient_descent(network, train_x, train_y, test_x, test_y, epochs):
-    indices = cp.arange(0, train_x.shape[0], 1)
+    indices = np.arange(0, train_x.shape[0], 1)
     for epoch in range(epochs):
-        cp.random.shuffle(indices)
+        np.random.shuffle(indices)
         train_x, train_y = train_x[indices], train_y[indices]
         for x, y in zip(train_x, train_y):
             x, y = network.reshape_data(x, y)
             a, z = network.forward_prop(x)
             weight_gradient, bias_gradient = network.back_prop(a, z, y)
             network.update_weights_and_biases(weight_gradient, bias_gradient)
-        print(f'Epoch: {epoch + 1}. Accuracy: {network.test_accuracy(test_x, test_y) * 100}%')
+        acc = network.test_accuracy(test_x, test_y) 
+        print(f'Epoch: {epoch + 1}. Accuracy: {acc * 100}%')
+        network.history.append(acc)
     
 def mini_batch_gradient_descent(network, train_x, train_y, test_x, test_y, epochs, mini_batch_size = 30):
     num_batches = train_x.shape[0] / mini_batch_size 
-    indices = cp.arange(0, train_x.shape[0], 1)
+    indices = np.arange(0, train_x.shape[0], 1)
     for epoch in range(epochs):
-        cp.random.shuffle(indices)
+        np.random.shuffle(indices)
         train_x, train_y = train_x[indices], train_y[indices]
-        split_x, split_y = cp.split(train_x, num_batches), cp.split(train_y, num_batches)
+        split_x, split_y = np.split(train_x, num_batches), np.split(train_y, num_batches)
         for batch_x, batch_y in zip(split_x, split_y):
-            total_weight_gradient = [cp.zeros(w.shape) for w in reversed(network.weights)]
-            total_bias_gradient = [cp.zeros(b.shape) for b in reversed(network.biases)]
+            total_weight_gradient = [np.zeros(w.shape) for w in reversed(network.weights)]
+            total_bias_gradient = [np.zeros(b.shape) for b in reversed(network.biases)]
             for x, y in zip(batch_x, batch_y):
                 x, y = network.reshape_data(x, y)
                 a, z = network.forward_prop(x)
@@ -51,5 +57,7 @@ def mini_batch_gradient_descent(network, train_x, train_y, test_x, test_y, epoch
                 total_weight_gradient[i] /= batch_x.shape[0]
                 total_bias_gradient[i] /= batch_x.shape[0]
             network.update_weights_and_biases(total_weight_gradient, total_bias_gradient)
-        print(f'Epoch: {epoch + 1}. Accuracy: {network.test_accuracy(test_x, test_y) * 100}%')
+        acc = network.test_accuracy(test_x, test_y) 
+        print(f'Epoch: {epoch + 1}. Accuracy: {acc * 100}%')
+        network.history.append(acc)
     
